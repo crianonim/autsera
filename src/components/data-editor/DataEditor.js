775e4a172909fe00c09633e13d9b7fa1b9cfbc15
Jan Skowronski <crianonim@gmail.com>
2019-10-05 10:40:51 +0100

    Add new Place

--- src/components/data-editor/DataEditor.js
+++ src/components/data-editor/DataEditor.js
@@ -11,10 +11,34 @@ export default ({ dataSource }) => {
   };
   return (
     <div className="editor">
+      <button
+        onClick={() => {
+          setData({
+            ...data,
+            places: data.places.concat({
+              id: data.meta.sequence++,
+              displayName:"",
+              name: "newPlace",
+              starsToUnlock: 0,
+              image: "playground.png",
+              positionOnMap: [0, 0],
+              interactions: []
+            })
+          });
+        }}
+        className="addNew"
+      >
+        Add New Place
+      </button>
       {data.places.map(place => (
         <Place key={place.id} setPlace={setPlace} place={place} />
       ))}
-      <textarea readOnly style={{width:"100%",height:"1100px"}} value={"export default "+JSON.stringify(data,null," ")}></textarea>}
+      <textarea
+        readOnly
+        style={{ width: "100%", height: "1100px" }}
+        value={"export default " + JSON.stringify(data, null, " ")}
+      ></textarea>
+      }
     </div>
   );
 };
